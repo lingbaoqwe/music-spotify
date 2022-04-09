3d174dbdf83f66ce81192e7f3f39701360a0e6ae
lingbaoqwe <52990132+lingbaoqwe@users.noreply.github.com>
2022-04-09 12:07:53 -0400

    test3

--- src/app/favourites/favourites.component.ts
+++ src/app/favourites/favourites.component.ts
@@ -31,26 +31,3 @@ export class FavouritesComponent implements OnInit {
     this.favouriteList.unsubscribe();
   }
 }
-
-// import { Component, OnInit } from '@angular/core';
-// import { MusicDataService } from '../music-data.service';
-
-// @Component({
-//   selector: 'app-favourites',
-//   templateUrl: './favourites.component.html',
-//   styleUrls: ['./favourites.component.css'],
-// })
-// export class FavouritesComponent implements OnInit {
-//   favourites: Array<any> = [];
-
-//   constructor(private msdata: MusicDataService) {}
-
-//   ngOnInit() {
-//     this.msdata.getFavourites().subscribe(data => (this.favourites = data.tracks));
-//   }
-
-//   removeFromFavourites(id: string) {
-//     this.msdata.removeFromFavourites(id).subscribe((data) => (this.favourites = data.tracks));
-//   }
-
-// }
